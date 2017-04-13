3d2867bb4849488c112197fe4c98f3a8e70d6b1c
michael-wise <m.lee.wise@gmail.com>
2017-04-12 20:19:17 -0400

    More convenient to set environment variable for eventbrite api key in a ruby file than in shell due to docker limitations.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ActiveRecord
+	# BUG! before you can create a new user using /signup, you need you 
+	# comment out before_action :authorize, then use /signup to create a 
+	# new user, then uncomment before_action :authorize. Remember to 
+	# remove all routes for user signup afterwards so people don't get 
+	# access to edit things! Later this will be solved with admin flags.
 	before_action :authorize
 	#A wonderful get-going-fast guide on bcrypt auth in rails.
 	# => https://gist.github.com/thebucknerlife/10090014
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
 require_relative 'application'
-
+app_environment_variables = File.join(Rails.root, 'config', 'environment_variables.rb')
+if File.exists?(app_environment_variables) 
+	load(app_environment_variables) 
+else 
+	puts "please set ENV['eventbritePersonal']='your api key' in config/environment_variables.rb"
+end
 # Initialize the Rails application.
 Rails.application.initialize!
